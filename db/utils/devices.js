3308420482d832434bf5670d5d363c9a3f3e35c6
Farolan Faisal <farolanfaisal@gmail.com>
2019-09-04 07:35:46 +0800

    update device generation

--- db/utils/devices.js
+++ db/utils/devices.js
@@ -2,38 +2,36 @@ const _ = require('lodash')
 const faker = require('faker')
 
 const families = ['FAM1', 'FAM2', 'FAM3']
-const models = ['MODEL1', 'MODEL2', 'MODEL3', 'MODEL4']
+const models = ['PXM2260', 'PXG900']
+const deviceStatuses = ['Configured', 'Unconfigured', 'CheckOut', 'Issue']
+const issues = ['Device offline', 'Timed out', 'A firmware update is required', 'Unknown error']
+const compartments = _.times(5, () => _.upperFirst(faker.internet.domainWord()))
 
-const deviceStatuses = ['unconfigured', 'configured', 'error', 'checkedOut', 'configuring']
-
-const generateDeviceName = () => ['PXM', 'PXG'][faker.random.number() % 2] + faker.random.number() % 1000
+const sample = arr => arr[faker.random.number() % arr.length]
 
 const generateDevice = orderItemIds => () => {
-  const status = deviceStatuses[faker.random.number() % (deviceStatuses.length - 1)]
-  const ip = faker.internet.ip()
-  const subnet = ip.substring(0, ip.lastIndexOf('.')) + '.0'
+  const status = sample(deviceStatuses)
   return {
-    orderItemId: orderItemIds[faker.random.number() % orderItemIds.length],
-    devicePhoto: faker.image.technics(),
-    deviceName: generateDeviceName(),
-    deviceIssue: '',
-    status,
+    orderItemId: sample(orderItemIds),
     deviceId: faker.random.uuid().substring(0, 4),
+    devicePhoto: faker.image.technics(),
+    deviceIssue: status === 'Issue' ? sample(issues) : '',
+    status,
     location: `Structure ${faker.random.number() % 10} - Compartment Name`,
-    family: families[faker.random.number() % families.length],
-    model: models[faker.random.number() % models.length],
+    compartment: sample(compartments),
+    family: sample(families),
+    model: sample(models),
     styleNumber: faker.random.number() % 100,
-    configFilename: status === 'configured' ? faker.lorem.word() + faker.random.number() % 1000 + '.bin' : null,
-    compartment: faker.internet.domainWord(),
-    labelName: '',
-    name: '',
+    configFilename: status === 'Configured' || status === 'CheckOut' ? faker.lorem.word() + faker.random.number() % 1000 + '.xml' : null,
+    labelName: faker.internet.domainWord().substring(0, 2).toUpperCase() + 'Label.pdf',
+    name: _.upperFirst(faker.internet.domainWord()),
     ctPri: faker.random.number() % 9000 + 1000,
     ctSec: faker.random.number() % 50,
     ptPri: faker.random.number() % 900 + 100,
     ptSec: faker.random.number() % 150,
-    wiring: '',
-    ip,
-    subnet
+    wiring: `${faker.random.number() % 3 + 1}-phase, ${faker.random.number() % 3 + 1}-wire`,
+    ip: faker.internet.ip(),
+    subnet: '255.255.255.0'
   }
 }
 
