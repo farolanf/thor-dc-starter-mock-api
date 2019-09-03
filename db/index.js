b373c4e2034cda068877cac27eb9b746ca420275
Farolan Faisal <farolanfaisal@gmail.com>
2019-09-03 21:59:41 +0800

    add orders

--- db/index.js
+++ db/index.js
@@ -1,3 +1,9 @@
+const faker = require('faker')
+
+// generate same data between runs
+faker.seed(123)
+
 module.exports = {
-    users: require('./users')
+  users: require('./users'),
+  orders: require('./orders')
 }
