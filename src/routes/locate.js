03e3d10fa41cd6f9cbcf61c0c6287eb88a4decbb
Farolan Faisal <farolanfaisal@gmail.com>
2019-09-04 21:35:56 +0800

    add totals

--- src/routes/locate.js
+++ src/routes/locate.js
@@ -36,21 +36,29 @@ const search = (data, query) => {
   return data
 }
 
+const isConfigured = device => device.status === 'Configured' || device.status === 'CheckOut'
+
 module.exports = server => {
   // Search devices by some criteria.
   // Filtering nested array values and nested object injected by _embed or _expand
   // is not supported by json-server, so we need to write our own.
   server.get('/locate', (req, res) => {
-    let data = _.cloneDeep(orderItems)
     // filter orderItems
-    data = filter(data, _.pick(req.query, ['orderNumber', 'itemNumber']))
-    data.forEach(orderItem => {
+    const filteredOrderItems = filter(orderItems, _.pick(req.query, ['orderNumber', 'itemNumber']))
+    filteredOrderItems.forEach(orderItem => {
       const query = Object.assign({}, req.query, { orderItemId: orderItem.id })
       // filter devices by full match params
       orderItem.devices = filter(devices, _.pick(query, ['orderItemId', 'model', 'deviceId', 'family', 'compartment', 'ip', 'subnet']))
       // filter devices by full text search
       orderItem.devices = search(orderItem.devices, _.pick(req.query, ['location']))
     })
-    res.json(data)
+    const foundDevices = _.flatMap(filteredOrderItems, o => o.devices)
+    const configuredCount = _.filter(foundDevices, isConfigured).length
+    res.json({
+      totalDevices: foundDevices.length,
+      configuredCount,
+      unconfiguredCount: foundDevices.length - configuredCount,
+      devices: foundDevices
+    })
   })
 }
